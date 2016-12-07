500c8ca1ff204c3857ef679c15714f8a48a958eb
Ryan L <supersaiyanblue@users.noreply.github.com>
2016-12-06 21:42:32 -0700

    Added ROT13 challenge - pseudocode only, no solution

---
+++ rot13.js
@@ -0,0 +1,22 @@
+
+function rot13(str) { // LBH QVQ VG!
+  //For each character, 
+  
+  //unless it is a space or punctuation,
+
+  //Get the unicode value
+  
+  //If that value is greater than or equal to 65 and less than or equal to 77
+  
+  //Add 13 to the value
+  
+  //If the value is greater than or equal to 78 and less than or equal to 90
+  
+  //Subtract 13 from the value
+  
+  //Then render a new character using the new value
+  return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
