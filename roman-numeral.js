96d50d573bcf3f130f9266fd57e7fc9db9053480
Ryan L <codelinde@users.noreply.github.com>
2017-01-12 13:08:24 -0700

    Update roman-numeral.js

--- roman-numeral.js
+++ roman-numeral.js
@@ -1,42 +1,33 @@
-var conversions = {
-	1: "I",
-	2: "II",
-	3: "III",
-	5: "V",
-	10: "X",
-	50: "L",
-	100: "C",
-	500: "D",
-	1000: "M"
+var conversions = [
+	[1, "I"],
+	[4, "IV"],
+	[5, "V"],
+	[9, "IX"],
+	[10, "X"],
+	[40, "XL"],
+	[50, "L"],
+	[90, "XC"],
+	[100, "C"],
+	[400, "CD"],
+	[500, "D"],
+	[900, "CM"],
+	[1000, "M"]
+];
+
+var counter = 0;
+var total = "";
+
+function romanize(num) {
+  var iterator = num;
+  for (var i in conversions) {
+	  if (conversions[i][0] > iterator) {
+		  total += conversions[i-1][1];
+		  counter += conversions[i-1][0];
+		  iterator -= conversions[i-1][0];
+		  romanize(iterator);
+	  }
+  }
+  return total;
 }
 
-function convertToRoman(num) {
-	if (num == 0) {
-		return "Please enter a number between 1 and 3,999.";
-	}
-	else if (num > 3999) {
-	  return "Please enter a number between 1 and 3,999.";
-	}
-	else {
-		if (num < 10) {
-			if (num < 4) {
-				var finalNum = conversions[num];
-			}
-			if (num > 3) {
-				// Apply generic logic which should apply to all cases
-			}
-		}
-		else if (num > 9) {
-			// Convert double-digit number to string
-			var strNum = num.toString();
-			// Split string into an array
-			var arrNum = strNum.split('');
-		}
-	}
-
-
-
- return finalNum;
-}
-
-convertToRoman(3);
+romanize(25);
