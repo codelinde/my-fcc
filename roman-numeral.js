832fb140c03b5f45cd78568b225283e6446d8be0
Ryan L <codelinde@users.noreply.github.com>
2017-01-13 20:31:06 -0700

    Update roman-numeral.js

--- roman-numeral.js
+++ roman-numeral.js
@@ -1,33 +1,29 @@
-var conversions = [
-	[1, "I"],
-	[4, "IV"],
-	[5, "V"],
-	[9, "IX"],
-	[10, "X"],
-	[40, "XL"],
-	[50, "L"],
-	[90, "XC"],
-	[100, "C"],
-	[400, "CD"],
-	[500, "D"],
-	[900, "CM"],
-	[1000, "M"]
-];
-
-var counter = 0;
-var total = "";
-
 function romanize(num) {
-  var iterator = num;
-  for (var i in conversions) {
-	  if (conversions[i][0] > iterator) {
-		  total += conversions[i-1][1];
-		  counter += conversions[i-1][0];
-		  iterator -= conversions[i-1][0];
-		  romanize(iterator);
-	  }
-  }
-  return total;
+	var conversions = [
+		[1000,"M"],
+	 	[900, "CM"],
+	 	[500, "D"], 
+	 	[400, "CD"],
+	 	[100, "C"],
+	 	[90, "XC"],
+	 	[50, "L"],
+	 	[40, "XL"],
+	 	[10, "X"],
+	 	[9, "IX"],
+	 	[5, "V"],
+	 	[4, "IV"], 
+	 	[1, "I"]
+ 	];
+
+	var total = "";
+		for (var i in conversions) {
+			while(num >= conversions[i][0]) {
+				total += conversions[i][1];
+				num -= conversions[i][0];
+			}
+		}
+		return total;
 }
 
+//Test case
 romanize(25);
