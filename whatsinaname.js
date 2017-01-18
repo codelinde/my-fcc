1445e9789d779d564f13eed9b24bc0484a7399fe
Ryan L <codelinde@users.noreply.github.com>
2017-01-17 22:30:20 -0700

    Update whatsinaname.js

--- whatsinaname.js
+++ whatsinaname.js
@@ -1,11 +1,10 @@
 
-
 function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
   var sourceProp = Object.keys(source);
-  for(var i in sourceProp) {
+  for(var i in collection) {
   	if(collection[i].hasOwnProperty(sourceProp)) {
   		if(collection[i][sourceProp] == source[sourceProp]) {
   			arr.push(collection[i]);
@@ -17,5 +16,4 @@ function whatIsInAName(collection, source) {
   return arr;
 }
 
-
-whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
