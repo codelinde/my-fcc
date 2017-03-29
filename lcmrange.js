c3d1c04af83a5efda7ade506b0321be656bb411b
codelinde <codelinde@users.noreply.github.com>
2017-03-28 21:36:52 -0700

    updated code a bit

--- lcmrange.js
+++ lcmrange.js
@@ -1,32 +1,43 @@
 
 function smallestCommons(arr) {
-    // sort arguments from smallest to largest
+
     var sortedArgs = arr.sort(function(a, b) {
         return a - b;
     });
 
-    // create empty array to flesh out digits between args
     var newArr = [];
 
-    // set counter to first arg
     var counter = sortedArgs[0];
 
-    // set limit to second arg
-    var limit = sortedArgs[1];
+    var limit = sortedArgs.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
     
-    // push the first arg to the new array
     newArr.push(counter);
 
-    // fill out digits inbetween 
     while(counter < limit) {
         counter++;
         newArr.push(counter);
     }
-    
-    // reverse array to check if last two digits are correct answer
-    var reversedArr = newArr.slice().reverse();
+
+    var lowestPossible = limit;
+
+    var flag = true;
+
+    while(flag) {
+        function isDivisible(element) { 
+            return element % lowestPossible === 0;
+        }
+        if (newArr.every(isDivisible)) {
+            return lowestPossible;
+            flag = false;
+        }
+        else {
+            lowestPossible++;
+        }
+    }
 
 }
 
 
-smallestCommons([1,5]);
+smallestCommons([1,5,7]);
