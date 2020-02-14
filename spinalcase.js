e4f041b9cd823c7cfeb66390df9d508fe88e8e14
Ryan L <codelinde@gmail.com>
2020-02-14 09:17:52 -0700

    Update spinalcase.js
    Added a one line alternative to this function

--- spinalcase.js
+++ spinalcase.js
@@ -6,6 +6,9 @@ function spinalCase(str) {
   var dashesOnly = noCamel.replace(/[\s_-]/g, '-');
   var lowered = dashesOnly.toLowerCase();
   return lowered;
+  
+  //Could also be written on one line:
+  //return str.replace(/([a-z](?=[A-Z]))/g, '$1 ').replace(/[\s_-]/g, '-').toLowerCase();
 
 }
 
